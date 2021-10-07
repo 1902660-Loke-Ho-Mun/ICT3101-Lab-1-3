da0e5a94eb03697402431f42a30cb7aa654e81dc
1902660-Loke-Ho-Mun <1902660@sit.singaporetech.edu.sg>
2021-10-08 01:47:04 +0800

    Lab 4 Part 2
    Added Mock

--- ICT3101_Calculator.UnitTests/AdditionalCalculatorTests.cs
+++ ICT3101_Calculator.UnitTests/AdditionalCalculatorTests.cs
@@ -2,29 +2,39 @@
 using System.Collections.Generic;
 using System.Text;
 using ICT3101_Calculator_Lab;
+using Moq;
 using NUnit.Framework;
 
 namespace ICT3101_Calculator.UnitTests
 {
-    class MagicNumberUnitTests
+    public class AdditionalCalculatorTests
     {
         //lab 4 changed here
-        //added _fileReader in setup
+        //added MOCK in setup
+
+        //mock only used here
+        private Mock<IFileReader> _mockFileReader;
         private Calculator _calculator;
         public FileReader _fileReader;
         [SetUp]
         public void Setup()
         {
-            // Arrange
+            /*
+             * 2,-2,4,6,8,10,12,14
+             */
+            _mockFileReader = new Mock<IFileReader>();
+            _mockFileReader.Setup(fr =>
+            fr.Read(@"MagicNumbers.txt")).Returns(new string[8] { "2","-2","4","6","8","10","12","14" });
             _calculator = new Calculator();
-            _fileReader = new FileReader();
         }
 
         [Test]
         [TestCase(2)]
         public void GenMagicNum_WithPositiveInputAndPositiveNumberRetrieved_ResultEqualToTwoTimesItemRetrieved(int a)
         {
-            double result = _calculator.GenMagicNum(a, _fileReader);
+            double result = _calculator.GenMagicNum(a, _mockFileReader.Object);
+            
+            //double result = _calculator.GenMagicNum(a, _fileReader);
             Console.WriteLine(result);
             Assert.That((int)result, Is.EqualTo(8));
 
@@ -34,7 +44,10 @@ namespace ICT3101_Calculator.UnitTests
         [TestCase(1)]
         public void GenMagicNum_WithPositiveInputButNegativeNumberRetrieved_ResultEqualToNegativeTwoTimesItemRetrieved(int a)
         {
-            double result = _calculator.GenMagicNum(a, _fileReader);
+            double result = _calculator.GenMagicNum(a, _mockFileReader.Object);
+            
+            
+            //double result = _calculator.GenMagicNum(a, _fileReader);
             Console.WriteLine(result);
             Assert.That((int)result, Is.EqualTo(4));
 
@@ -44,7 +57,9 @@ namespace ICT3101_Calculator.UnitTests
         [TestCase(-1)]
         public void GenMagicNum_WithNegativeInput_ResultEqualToItemRetrieved(int a)
         {
-            double result = _calculator.GenMagicNum(a, _fileReader);
+            double result = _calculator.GenMagicNum(a, _mockFileReader.Object);
+
+            //double result = _calculator.GenMagicNum(a, _fileReader);
             Console.WriteLine(result);
             Assert.That((int)result, Is.EqualTo(0));
 
@@ -54,11 +69,11 @@ namespace ICT3101_Calculator.UnitTests
         [TestCase(20)]
         public void GenMagicNum_WithAnOutOfRangeInput_ResultEqualsToZero(int a)
         {
-            double result = _calculator.GenMagicNum(a, _fileReader);
+            double result = _calculator.GenMagicNum(a, _mockFileReader.Object);
+            //double result = _calculator.GenMagicNum(a, _fileReader);
             Console.WriteLine(result);
             Assert.That((int)result, Is.EqualTo(0));
 
         }
-
     }
 }
